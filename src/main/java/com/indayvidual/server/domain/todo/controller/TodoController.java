9d39002ca1cdf1768a4d2851ad7ce9b14fcdbb9f
eldeoddt <eldeoddt@gmail.com>
2025-07-13 01:46:17 +0900

    [FEAT] 카테고리 jwt 적용 #15

--- src/main/java/com/indayvidual/server/domain/todo/controller/TodoController.java
+++ src/main/java/com/indayvidual/server/domain/todo/controller/TodoController.java
@@ -4,11 +4,14 @@ import com.indayvidual.server.domain.todo.dto.request.CategoryCreateRequestDTO;
 import com.indayvidual.server.domain.todo.dto.response.CategoryResponseDTO;
 import com.indayvidual.server.domain.todo.service.CategoryCommandService;
 import com.indayvidual.server.domain.todo.service.CategoryQueryService;
+import com.indayvidual.server.domain.user.service.UserService.UserAuthService;
 import com.indayvidual.server.global.api.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,23 +19,25 @@ import java.util.List;
 @Tag(name = "Todo", description = "Todo API")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/todo/categories")
 public class TodoController {
 
     private final CategoryCommandService categoryCommandService;
     private final CategoryQueryService categoryQueryService;
+    private final UserAuthService userAuthService;
 
     @Operation(summary = "카테고리 등록", description = "새로운 카테고리를 등록합니다.")
     @PostMapping("")
     public ApiResponse<CategoryResponseDTO> createCategory(@RequestBody @Valid CategoryCreateRequestDTO request) {
-        Long userId = 2L; // TODO: JWT에서 사용자 ID 추출 예정
+        Long userId = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getName());
         return ApiResponse.onSuccess(categoryCommandService.create(request, userId));
     }
 
     @Operation(summary = "카테고리 목록 조회", description = "카테고리 목록을 조회합니다.")
     @GetMapping("")
     public ApiResponse<List<CategoryResponseDTO>> getCategories() {
-        Long userId = 2L; // TODO: JWT에서 사용자 ID 추출 예정
+        Long userId = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getName());
         return ApiResponse.onSuccess(categoryQueryService.findAll(userId));
     }
 }

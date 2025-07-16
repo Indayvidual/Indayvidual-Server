e2c02dd36d218dcf66ba87279fd0cefecd98fd55
박성준 <sungjunp1208@gmail.com>
2025-07-17 08:06:45 +0900

    [BUG] SwaggerConfig 수정 #29

--- src/main/java/com/indayvidual/server/global/config/SwaggerConfig.java
+++ src/main/java/com/indayvidual/server/global/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,5 +39,13 @@ public class SwaggerConfig {
                 .info(info)
                 .addSecurityItem(securityRequirement)
                 .components(components);
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("indayvidual-public")
+                .packagesToScan("dev.indayvidual.shop")
+                .build();
     }
 }
